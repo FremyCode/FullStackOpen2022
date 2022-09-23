80fdfd964ca8e0e2f12d62f99af8a6f539e5e34e
FremyCode <ville.pikaloff@gmail.com>
2022-09-23 10:23:44 +0300

    Fullstack osa 1
    Unicafe step 1

--- src/App.js
+++ src/App.js
@@ -1,68 +1,28 @@
-import React from "react"
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  }
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const Header = (props) => {
-    return (
-      <div>
-        <h1>{props.course}</h1>
-      </div>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <p>
-        {props.parts.name} {props.parts.exercises}
-      </p>
-    )
-  }
-
-  const Content = () => {
-    return (
-      <div>
-        <Part parts={course.parts[0]} exercises={course.parts[0].exercises} />
-        <Part parts={course.parts[1]} exercises={course.parts[1].exercises} />
-        <Part parts={course.parts[2]} exercises={course.parts[2].exercises} />
-      </div>
-    )
-  }
-
-  const Total = () => {
-    return (
-      <div>
-        <p>
-          Number of exercises{" "}
-          {course.parts[0].exercises +
-            course.parts[1].exercises +
-            course.parts[2].exercises}
-        </p>
-      </div>
-    )
-  }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Give feedback</h1>
+      <div>
+        <button onClick={() => setGood(good + 1)}>Good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+        <button onClick={() => setBad(bad + 1)}>Bad</button>
+      </div>
+      <div>
+        <div>
+          <h1>Statistics</h1>
+          <p>Good {good}</p>
+          <p>Neutral {neutral}</p>
+          <p>Bad {bad}</p>
+        </div>
+      </div>
     </div>
   )
 }
