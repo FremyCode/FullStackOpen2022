92d23e218fb0110d02401ff4cf806b82f5316325
FremyCode <ville.pikaloff@gmail.com>
2022-09-23 13:15:02 +0300

    Unicafe step 3
    Refaktoroi sovelluksesi siten, että tilastojen näyttäminen on eriytetty oman komponentin Statistics vastuulle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { useState } from "react";
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+      <h2>All {props.combinedCount}</h2>
+      <h2>Average {props.average}</h2>
+      <h2>Positive {props.positiveFeedback * 100}%</h2>
+    </div>
+  );
+};
+
 const App = () => {
-  // tallenna napit omaan tilaansa
+  
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const combinedCount = good + bad + neutral;
-  const average = (good*1 + neutral*0 + bad*-1) / combinedCount;
+  const average = (good * 1 + neutral * 0 + bad * -1) / combinedCount;
   const positiveFeedback = good / combinedCount;
-
-  //Laajenna sovellusta siten, että se näyttää palautteista enemmän statistiikkaa: yhteenlasketun määrän, keskiarvon
-  // (hyvän arvo 1, neutraalin 0, huonon -1) ja sen kuinka monta prosenttia palautteista on ollut positiivisia
-
 
   return (
     <div>
@@ -23,15 +33,14 @@ const App = () => {
         <button onClick={() => setBad(bad + 1)}>Bad</button>
       </div>
       <div>
-        <div>
-          <h1>Statistics</h1>
-          <p>Good {good}</p>
-          <p>Neutral {neutral}</p>
-          <p>Bad {bad}</p>
-          <h2>All {combinedCount}</h2>
-          <h2>Average {average}</h2>
-          <h2>Positive {positiveFeedback * 100}%</h2>
-        </div>
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          combinedCount={combinedCount}
+          average={average}
+          positiveFeedback={positiveFeedback}
+        />
       </div>
     </div>
   );
