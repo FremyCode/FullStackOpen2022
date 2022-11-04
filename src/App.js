b9edb7e4bbe027986155a0cc93341e48321002f6
FremyCode <ville.pikaloff@gmail.com>
2022-11-04 14:11:34 +0200

    Divided "Course" into own component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
@@ -46,43 +47,12 @@ const App = () => {
     },
   ]
 
-  const Header = props =>
-  <h1>{props.course.name}</h1>
-
-  const Total = (props) => {
-    const parts = props.course.parts.map(course => course.exercises)
-
-    return (
-      <h3>Total of {parts.reduce((s, p) => s + p)} exercises</h3>
-    )
-  }
-
-  const Part = props =>
-    <p>{props.name} {props.exercises}</p>
-
-  const Content = (props) => {
-      return (
-          <div>
-              {props.course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}  
-          </div>
-      )
-  }
-  const Course = (props) => {  
-      console.log(props)  
-
-      return (
-          <div>
-              <Header course={props.course} />
-              <Content course={props.course} />
-              <Total course={props.course} />
-          </div>
-      )
-  }
-
   return (
     <div>
       <h1>Web development curriculum</h1>
-      {courses.map(course => <Course key = {courses.id} course={course} />)}
+      {courses.map((course) => (
+        <Course key={courses.id} course={course} />
+      ))}
     </div>
   )
 }
