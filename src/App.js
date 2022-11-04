7409876cfb459d474f86645dda33f2ec79262cd3
FremyCode <ville.pikaloff@gmail.com>
2022-11-04 14:05:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,34 +49,34 @@ const App = () => {
   const Header = props =>
   <h1>{props.course.name}</h1>
 
-const Total = (props) => {
-  const parts = props.course.parts.map(course => course.exercises)
-
-  return (
-    <p>Total of {parts.reduce((s, p) => s + p)} exercises</p>
-  )
-}
-
-const Part = props =>
-  <p>{props.name} {props.exercises}</p>
-
-const Content = (props) => {
-    return (
-        <div>
-            {props.course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}  
-        </div>
-    )
-}
-const Course = (props) => {  
-    console.log(props)  
+  const Total = (props) => {
+    const parts = props.course.parts.map(course => course.exercises)
 
     return (
-        <div>
-            <Header course={props.course} />
-            <Content course={props.course} />
-            <Total course={props.course} />
-        </div>
+      <h3>Total of {parts.reduce((s, p) => s + p)} exercises</h3>
     )
+  }
+
+  const Part = props =>
+    <p>{props.name} {props.exercises}</p>
+
+  const Content = (props) => {
+      return (
+          <div>
+              {props.course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}  
+          </div>
+      )
+  }
+  const Course = (props) => {  
+      console.log(props)  
+
+      return (
+          <div>
+              <Header course={props.course} />
+              <Content course={props.course} />
+              <Total course={props.course} />
+          </div>
+      )
   }
 
   return (
