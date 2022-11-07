cf7b694060aa7375708f7bb20bb5451b240a529b
FremyCode <47107009+FremyCode@users.noreply.github.com>
2022-11-07 15:17:12 +0200

    Puhelinluettelo step 1
    Yksinkertainen useState array listan päivitys nappia painamalla

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+
+  const [newName, setNewName] = useState("")
 
   const addName = (event) => {
+
     event.preventDefault()
+    setPersons([...persons, {name: newName}])
 
   }
-
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+           />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={addName} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map( person => 
-          person={person}
-          )}
-      </ul>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   )
-
 }
 
 export default App
