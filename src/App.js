1499580c84cf950f7927c7f990630c6cd8b31be8
FremyCode <ville.pikaloff@gmail.com>
2022-11-18 09:18:00 +0200

    Added db.json and Axios
    Persons data is now fetched from db.json with Axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react"
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ])
+  
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((res) => {
+      setPersons(res.data)
+    })
+  }, [])
 
   const [newName, setNewName] = useState("")
 
@@ -89,7 +92,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow}/>
+      <Persons personsToShow={personsToShow} />
     </div>
   )
 }
